e69c86ee7deea6e9dd0e3d577a9ffd57cf35fb95
Shubh Gupta <96350011+Quietcod@users.noreply.github.com>
2022-10-14 01:36:32 +0530

    Update network_scanner.py

--- network_scanner.py
+++ network_scanner.py
@@ -15,7 +15,7 @@ def scan(ip):
     arp_request = scapy.ARP(pdst=ip)
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
     arp_request_broadcast = broadcast / arp_request
-    print(arp_request_broadcast.summary())
+#     print(arp_request_broadcast.summary())
     answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
     client_list = []
 
